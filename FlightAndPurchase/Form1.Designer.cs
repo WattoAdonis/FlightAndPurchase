5c7f3d6491ecbd0d3e54b119e5eb1c62e52296e5
Anthony Mousoulis <anthony.mousoulis2015@hotmail.com>
2016-05-12 11:19:55 -0400

    First Build Of Team Project Assignment

--- FlightAndPurchase/Form1.Designer.cs
+++ FlightAndPurchase/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlightAndPurchase
 {
-    partial class Form1
+    partial class FlightForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlightForm));
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtBoxDestination = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSignIn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1253, 28);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDestination.Location = new System.Drawing.Point(63, 87);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(62, 15);
+            this.lblDestination.TabIndex = 1;
+            this.lblDestination.Text = "Destination";
+            // 
+            // txtBoxDestination
+            // 
+            this.txtBoxDestination.Location = new System.Drawing.Point(163, 84);
+            this.txtBoxDestination.Name = "txtBoxDestination";
+            this.txtBoxDestination.Size = new System.Drawing.Size(324, 20);
+            this.txtBoxDestination.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Flight 01:",
+            "Flight 02:",
+            "Flight 03:",
+            "Flight 04:",
+            "Flight 05:",
+            "Flight 06:",
+            "Flight 07:",
+            "Flight 08:",
+            "Flight 09:",
+            "Flight 10:"});
+            this.listBox1.Location = new System.Drawing.Point(163, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(324, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(901, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "E-mail address:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1004, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lblSignIn
+            // 
+            this.lblSignIn.AutoSize = true;
+            this.lblSignIn.Location = new System.Drawing.Point(1004, 47);
+            this.lblSignIn.Name = "lblSignIn";
+            this.lblSignIn.Size = new System.Drawing.Size(83, 13);
+            this.lblSignIn.TabIndex = 6;
+            this.lblSignIn.Text = "Account Sign-In";
+            // 
+            // FlightForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = global::FlightAndPurchase.Properties.Resources.glider_h6w;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1355, 625);
+            this.Controls.Add(this.lblSignIn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtBoxDestination);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.btnExit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FlightForm";
+            this.Text = "Flight Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtBoxDestination;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSignIn;
     }
 }
 
--- FlightAndPurchase/Form1.cs
+++ FlightAndPurchase/Form1.cs
@@ -1,4 +1,45 @@
-﻿using System;
+﻿/*
+Team members and the contributions that each has been assigned.
+    Team Name:
+    Project Name:
+    Project Manager:    Lambert Munyawera   857-244-5746    lmunyawera94@gmail.com
+
+                        Anthony Mousoulis   603-727-6422    anthony.mousoulis2015@hotmail.com
+                        Leiyanka Daellani   571-699-8127    daellaniP9@gmail.com                FAILED DAEMONED MAIL
+                        Kola Yves   		240-423-2539    yvris993@gmail.com
+                        Sameal Molla		202-390-8865    sammydmolla@gmail.com
+                        Welton Tyler		434-594-5735    wtyler1@gmail.com
+                        
+    Team evaluators and class instructors:
+                        Vasu                703-855-7426	vasu@ispaninc.com
+                        Nancy               703-597-3098	nancy@ispaninc.com
+                        Karishma                    		karishma@ebsolutioninc.com
+*/
+// FlightExpedia is an example refference to guide us...
+/*
+    Requirements:       Part 01:    At least 10 flight options
+                        Part 02:    Sorted by Price
+                                    Sorted by Duration of flight
+                                    Sorted by number of Stops/Connections
+                        Part 03:    Show flight information and send a confirmation by e-mail containing:
+                                        One way as well as return flight
+                                        Airline
+                                        Departure time
+                                        Arrival time
+                                        Duration flight
+                                        Number of stops
+                                        Price
+    Date started:       Wednesday May 12, 2016
+    Due date:           Friday May 13, 2016 Documentation, Wireframe Layout.
+                        Monday May 13, 2016 User Interface.
+*/
+
+/*
+image retrieved from "Free Images"  http://www.bigfoto.com/themes/aviation/glider-h6w.jpg
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +51,46 @@ using System.Windows.Forms;
 
 namespace FlightAndPurchase
 {
-    public partial class Form1 : Form
+    public partial class FlightForm : Form
     {
-        public Form1()
+        #region Part 01:    At least 10 flight options
+        #endregion
+
+        #region Part 02:
+        /*
+            Sorted by Price
+            Sorted by Duration of flight
+            Sorted by number of Stops/Connections
+        */
+        #endregion
+
+        #region Part 03:
+        /*
+            Show flight information and send a confirmation by e-mail containing:
+                One way as well as return flight
+                Airline
+                Departure time
+                Arrival time
+                Duration flight
+                Number of stops
+                Price
+        */
+        #endregion
+
+        public FlightForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form and exit
+            Application.Exit();
         }
     }
 }
